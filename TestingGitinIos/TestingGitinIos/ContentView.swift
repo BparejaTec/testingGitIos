99eb3d7c13ee25d9110ed330a33762073216f4d9
me7aben <bpareja.me@gmail.com>
2025-10-20 22:11:28 -0500

    cambio desde main

--- TestingGitinIos/TestingGitinIos/ContentView.swift
+++ TestingGitinIos/TestingGitinIos/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Alumnos!")
+            Text("Hello, desde la rama principal!")
         }
         .padding()
     }
