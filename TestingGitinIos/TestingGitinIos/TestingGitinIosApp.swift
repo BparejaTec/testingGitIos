805d3387c5aa1bbb2af0efc51e37ac58ab762d61
me7aben <bpareja.me@gmail.com>
2025-10-20 21:59:06 -0500

    Creación del proyecto

---
+++ TestingGitinIos/TestingGitinIos/TestingGitinIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGitinIosApp.swift
+//  TestingGitinIos
+//
+//  Created by Brian Benjamin Pareja Meruvia on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TestingGitinIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
